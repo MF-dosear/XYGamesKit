f31c863112b4c8c870b41c8be38d5ceb50964b29
Paul <564057354@qq.com>
2023-11-24 10:17:44 +0800

    xiugai

--- Example ObjC/XYGamesDemo/File.swift
+++ Example ObjC/XYGamesDemo/File.swift
@@ -15,3 +15,57 @@ class XYModel: NSObject {
 //        XYGamesKit.init()
     }
 }
+
+//Pod::Spec.new do |s|
+//  s.name             = 'XYGamesKit'
+//  s.platform         = :ios, '12.0'
+//  s.version          = '1.0.0'
+//  s.summary          = 'XYGamesKit.'
+//
+//  s.description      = <<-DESC
+//  XYGamesKit 游戏 SDK.
+//                       DESC
+//
+//  s.homepage         = 'https://github.com/MF-dosear/XYGamesKit'
+//  s.license          = { :type => 'MIT', :file => 'LICENSE' }
+//  s.author           = { 'Paul' => 'dosear@qq.com' }
+//  s.source           = { :git => 'https://github.com/MF-dosear/XYGamesKit.git', :tag => s.version.to_s }
+//
+//  s.ios.deployment_target = '12.0'
+//
+//  s.requires_arc = true
+//  
+//  # 指定 Swift 语言版本，Swift 项目需要设置这个
+//  s.swift_version = ['5']
+//  
+//  #系统库
+//  s.frameworks  = 'CoreFoundation','SystemConfiguration','CFNetwork','UIKit','StoreKit','CoreTelephony','Security','AdSupport'
+//  
+//  # 系统libs
+//  s.libraries = 'z','sqlite3','c++'
+//
+//
+//  # bundle资源
+//  s.resource_bundles = {
+//      'XYGamesKit' => ['XYGamesKit/Assets/*']
+//  }
+//  
+//  s.source_files = 'XYGamesKit/Classes/**/*','XYGamesKit/Libraries/**/*.h'
+//  
+//#  s.public_header_files = ['XYGamesKit/Classes/**/*','XYGamesKit/Libraries/**/*.h']
+//
+//  # 内置libs路径
+//  s.vendored_libraries = 'XYGamesKit/Libraries/**/*.a'
+//
+//  # 第三方库和资源
+//  s.vendored_frameworks = 'XYGamesKit/Frameworks/**/*.framework'
+//  s.resources = ['XYGamesKit/Frameworks/**/*.bundle','XYGamesKit/Libraries/**/*.bundle']
+//  
+//  # pod第三方库
+//  s.dependency 'SVProgressHUD','~> 2.3.1'
+//  s.dependency 'CryptoSwift','~> 1.8.0'
+//  s.dependency 'SnapKit','~> 5.6.0'
+//  s.dependency 'RxSwift','~> 6.6.0'
+//  s.dependency 'RxCocoa','~> 6.6.0'
+//
+//end
